91adb6d9a4f816004e335d5ec699c962190db198
M Rota <manuel.rota@ymail.com>
2016-09-08 23:54:48 +0200

    Posterizing (kinda)

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 
 /*
  * TODO:
- *	- merge matrices and save "posterized" image
+ *	- DUNNO :(
  */
 
 
@@ -22,7 +22,8 @@ void clusters_to_bmp(int *cluster_map, int k, int height, int width, char *filen
 unsigned char * merge_clusters(int *cluster_map, int k, int height, int width, char *filename);
 void test_split(char *filename, int k);
 void test_posterize(char *filename, int k);
-
+void reorder_cluster(unsigned char *matrix, int *cluster_map, int height, int width, int k);
+int find(int element, int *array, int size);
 
 int valid_file(char *filename)
 {
@@ -48,7 +49,7 @@ int * kmeans_bmp(struct BMP *bmp, int k)
 	 * arbitrary stretch factor, need further investigation on optimal value
 	 * (probably correlated to height and width)
 	 */
-	int stretch = 100;
+	int stretch = (width+height)*k;
 	for (int i=0 ; i<height ; ++i) {
 		for (int j=0 ; j<width ; ++j) {
 			space[(i*width + j)*3 + 0] = (long) i;
@@ -58,10 +59,93 @@ int * kmeans_bmp(struct BMP *bmp, int k)
 	}
 
 	int *cluster_map = kmeans(space, width*height, 3, k);
+	reorder_cluster(matrix, cluster_map, height, width, k);
 
 	free(space);
 
 	return cluster_map;
+}
+
+
+/*
+ * Reorder cluster_map (IN PLACE!) by comparing first seen levels from the original matrix
+ */
+void reorder_cluster(unsigned char *matrix, int *cluster_map, int height, int width, int k)
+{
+	int *new_cs = malloc(k*sizeof(int));
+	unsigned char *gray_v = malloc(k*sizeof(unsigned char));
+	int n = 0;
+	int *seen = malloc(k*sizeof(int));
+
+	for (int n=0 ; n<k ; ++n)
+		seen[n] = -1;
+	
+	/* new_cs <- clusters' indexes
+	 * gray_v <- clusters' first picked value
+	 */
+	for (int i=0 ; i<height*width && n<k ; ++i) {
+		if (find(cluster_map[i], seen, k) < 0) {
+			new_cs[n] = cluster_map[i];
+			gray_v[n] = matrix[i];
+			seen[n] = cluster_map[i];
+			++n;
+		}
+	}
+
+	/* insertion sort to order clusters' values and the respective indexes */
+	for (int i=1 ; i<k ; ++i) {
+		unsigned char value = gray_v[i];
+		int cluster = new_cs[i];
+		int j = i-1;
+		while (j>=0 && gray_v[j] > value) {
+			gray_v[j+1] = gray_v[j];
+			new_cs[j+1] = new_cs[j];
+			j = j-1;
+		}
+		gray_v[j+1] = value;
+		new_cs[j+1] = cluster;
+	}
+
+	/* Convoluted things ahead (remember that clusters are now sorted!)
+	 *
+	 * pointers <- position in which each cluster's index appears
+	 * cluster_map <- new cluster index
+	 *
+	 * e.g.
+	 * 
+	 * Sorted clusters' indexes (new_cs)
+	 * {2, 0, 1}	(example values {0, 127, 255})
+	 * Pointers:
+	 * {1, 2, 0}
+	 *
+	 * New clusters' indexes: (assuming cluster_map = new_cs)
+	 * {0, 1, 2}
+	 * 
+	 * Old cluster was 2, pointers[2]=0 which is the new cluster's index
+	 * Old cluster was 0, pointers[0]=1 which is the new cluster's index
+	 * Old cluster was 1, pointers[1]=2 which is the new cluster's index
+	 */
+
+	int *pointers = malloc(k*sizeof(int));
+	for (int n=0 ; n<k ; ++n)
+		pointers[new_cs[n]] = n;
+
+	for (int i=0 ; i<height*width ; ++i)
+		cluster_map[i] = pointers[cluster_map[i]];
+
+	free(new_cs);
+	free(gray_v);
+	free(pointers);
+}
+
+
+int find(int element, int *array, int size)
+{
+	for (int i=0 ; i<size ; ++i) {
+		if (element==array[i])
+			return i;
+	}
+	return -1;
 }
 
 
@@ -119,10 +203,10 @@ unsigned char * merge_clusters(int *cluster_map, int k, int height, int width, c
 
 	unsigned char *gray_levels = malloc(k*sizeof(unsigned char));
 	
-	float level = max;
+	float level = min;
 	for (int i=0 ; i<k ; ++i) {
 		gray_levels[i] = (unsigned char) round(level);
-		level -= step;
+		level += step;
 	}
 
 	unsigned char *matrix = malloc(height*width*(sizeof(unsigned char)));
